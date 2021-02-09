db3bdb991ba615d049a8f7ac0ff75316c95ea3f6
Erdvik97 <magnuserdvik@hotmail.com>
2021-02-09 10:57:54 +0100

    la til modeller

---
+++ EgdeBookingSystem/EgdeBookingSystem/Models/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EgdeBookingSystem.Models
+{
+    public class Booking
+    {
+        public int ID { get; set; }
+        
+        public Utstyr Utstyr { get; set; }
+        public BookingTime BookingTime { get; set; }
+
+        //public Customer Customer { get; set; }
+    }
+}
---
+++ EgdeBookingSystem/EgdeBookingSystem/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EgdeBookingSystem.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public int Email { get; set; }
+    
+    }
+}
